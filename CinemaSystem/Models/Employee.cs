4950f81aaac331a703cff048c4a86c9f42de9b88
Максим Серов <maximkvnsk@gmail.com>
2020-12-01 19:45:09 +0200

    Revert "1121Добавьте GITIGNORE- и GITATTRIBUTES-файлы."
    This reverts commit 92ef13bfab24a5baf1c608cdd08f96e05435b20e.

---
+++ CinemaSystem/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace CinemaSystem.Models
+{
+
+ 
+    public partial class Employee
+    {
+        public Employee()
+        {
+            Reservations = new HashSet<Reservation>();
+        }
+
+       [Key]
+       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int? PostId { get; set; }
+
+        public virtual Post Post { get; set; }
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
